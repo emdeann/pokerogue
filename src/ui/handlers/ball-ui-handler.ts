e0249a9c9ccc354eea84aa144d7f591f01dfb93b
Dean <69436131+emdeann@users.noreply.github.com>
2026-05-17 13:25:01 -0700

    Migrate ball ui handler

--- src/ui/handlers/ball-ui-handler.ts
+++ src/ui/handlers/ball-ui-handler.ts
@@ -2,18 +2,20 @@ import { globalScene } from "#app/global-scene";
 import { getPokeballName } from "#data/pokeball";
 import { Button } from "#enums/buttons";
 import { Command } from "#enums/command";
+import type { PokeballType } from "#enums/pokeball";
 import { TextStyle } from "#enums/text-style";
 import { UiMode } from "#enums/ui-mode";
 import type { CommandPhase } from "#phases/command-phase";
+import { ScrollableListHelper } from "#ui/scrollable-list-helper";
 import { addTextObject, getTextStyleOptions } from "#ui/text";
 import { UiHandler } from "#ui/ui-handler";
 import { addWindow } from "#ui/ui-theme";
 import i18next from "i18next";
 
 export class BallUiHandler extends UiHandler {
-  private pokeballSelectContainer: Phaser.GameObjects.Container;
   private pokeballSelectBg: Phaser.GameObjects.NineSlice;
   private countsText: Phaser.GameObjects.Text;
+  private listHelper: ScrollableListHelper<PokeballType>;
 
   private cursorObj: Phaser.GameObjects.Image | null;
 
@@ -27,36 +29,43 @@ export class BallUiHandler extends UiHandler {
     const ui = this.getUi();
 
     this.scale = getTextStyleOptions(TextStyle.WINDOW).scale;
+    const pbs = Array.from(Object.keys(globalScene.pokeballCounts)).map(s => Number.parseInt(s));
 
-    let optionsTextContent = "";
+    this.listHelper = new ScrollableListHelper(globalScene.scaledCanvas.width - 115, -672 * this.scale - 49, {
+      rows: pbs.length,
+      items: {
+        y: 9,
+        width: 96,
+        height: 72 * this.scale + 4,
+      },
+      cursor: {
+        texture: "cursor",
+        width: 6,
+        height: 10,
+        offsetX: 9,
+        offsetY: 2,
+      },
+      cancelText: i18next.t("commandUiHandler:ballCancel"),
+      textOptions: { lineSpacing: this.scale * 72 },
+      getLabel: pb => getPokeballName(pb),
+      textAlign: "right",
+      onItemActioned: pb => this.onPokeballAction(pb),
+      onCancel: () => this.onCancel(),
+    });
+    this.listHelper.setItems([...pbs]);
+    this.listHelper.setVisible(false);
 
-    for (let pb = 0; pb < Object.keys(globalScene.pokeballCounts).length; pb++) {
-      optionsTextContent += `${getPokeballName(pb)}\n`;
-    }
-    optionsTextContent += i18next.t("commandUiHandler:ballCancel");
-    const optionsText = addTextObject(0, 0, optionsTextContent, TextStyle.WINDOW, { align: "right", maxLines: 6 });
-    const optionsTextWidth = optionsText.displayWidth;
-    this.pokeballSelectContainer = globalScene.add.container(
-      globalScene.scaledCanvas.width - 51 - Math.max(64, optionsTextWidth),
-      -49,
-    );
-    this.pokeballSelectContainer.setVisible(false);
-    ui.add(this.pokeballSelectContainer);
-
-    this.pokeballSelectBg = addWindow(0, 0, 50 + Math.max(64, optionsTextWidth), 32 + 480 * this.scale);
-    this.pokeballSelectBg.setOrigin(0, 1);
-    this.pokeballSelectContainer.add(this.pokeballSelectBg);
-    this.pokeballSelectContainer.add(optionsText);
-    optionsText.setOrigin(0, 0);
-    optionsText.setPositionRelative(this.pokeballSelectBg, 42, 9);
-    optionsText.setLineSpacing(this.scale * 72);
-
+    this.pokeballSelectBg = addWindow(0, 0, 114, 672 * this.scale);
+    this.pokeballSelectBg.setOrigin(0, 0);
+    this.listHelper.add(this.pokeballSelectBg);
+    this.listHelper.sendToBack(this.pokeballSelectBg);
     this.countsText = addTextObject(0, 0, "", TextStyle.WINDOW, {
       maxLines: 5,
     });
     this.countsText.setPositionRelative(this.pokeballSelectBg, 18, 9);
     this.countsText.setLineSpacing(this.scale * 72);
-    this.pokeballSelectContainer.add(this.countsText);
+    this.listHelper.add(this.countsText);
+    ui.add(this.listHelper);
 
     this.setCursor(0);
   }
@@ -65,45 +74,45 @@ export class BallUiHandler extends UiHandler {
     super.show(args);
 
     this.updateCounts();
-    this.pokeballSelectContainer.setVisible(true);
+    this.listHelper.setVisible(true);
     this.setCursor(this.cursor);
 
     return true;
   }
 
+  private onCancel(): void {
+    this.getUi().setMode(UiMode.COMMAND, (globalScene.phaseManager.getCurrentPhase() as CommandPhase).getFieldIndex());
+  }
+
+  private onPokeballAction(pb: PokeballType): boolean {
+    if (globalScene.pokeballCounts[pb] <= 0) {
+      this.getUi().playError();
+      return false;
+    }
+
+    const commandPhase = globalScene.phaseManager.getCurrentPhase() as CommandPhase;
+    if (commandPhase.handleCommand(Command.BALL, this.cursor)) {
+      globalScene.ui.setMode(UiMode.COMMAND, commandPhase.getFieldIndex());
+      globalScene.ui.setMode(UiMode.MESSAGE);
+      return true;
+    }
+
+    return false;
+  }
+
   processInput(button: Button): boolean {
     const ui = this.getUi();
-
     let success = false;
 
-    const pokeballTypeCount = Object.keys(globalScene.pokeballCounts).length;
-
-    if (button === Button.ACTION || button === Button.CANCEL) {
-      const commandPhase = globalScene.phaseManager.getCurrentPhase() as CommandPhase;
-      success = true;
-      if (button === Button.ACTION && this.cursor < pokeballTypeCount) {
-        if (globalScene.pokeballCounts[this.cursor]) {
-          if (commandPhase.handleCommand(Command.BALL, this.cursor)) {
-            globalScene.ui.setMode(UiMode.COMMAND, commandPhase.getFieldIndex());
-            globalScene.ui.setMode(UiMode.MESSAGE);
-            success = true;
-          }
-        } else {
-          ui.playError();
-        }
-      } else {
-        ui.setMode(UiMode.COMMAND, commandPhase.getFieldIndex());
+    switch (button) {
+      case Button.CANCEL:
+        this.onCancel();
         success = true;
-      }
-    } else {
-      switch (button) {
-        case Button.UP:
-          success = this.setCursor(this.cursor ? this.cursor - 1 : pokeballTypeCount);
-          break;
-        case Button.DOWN:
-          success = this.setCursor(this.cursor < pokeballTypeCount ? this.cursor + 1 : 0);
-          break;
-      }
+        break;
+      case Button.ACTION:
+      case Button.UP:
+      case Button.DOWN:
+        success = this.listHelper.processInput(button);
     }
 
     if (success) {
@@ -121,23 +130,9 @@ export class BallUiHandler extends UiHandler {
     );
   }
 
-  setCursor(cursor: number): boolean {
-    const ret = super.setCursor(cursor);
-
-    if (!this.cursorObj) {
-      this.cursorObj = globalScene.add.image(0, 0, "cursor");
-      this.pokeballSelectContainer.add(this.cursorObj);
-    }
-
-    this.cursorObj.setScale(this.scale * 6);
-    this.cursorObj.setPositionRelative(this.pokeballSelectBg, 12, 15 + (6 + this.cursor * 96) * this.scale);
-
-    return ret;
-  }
-
   clear() {
     super.clear();
-    this.pokeballSelectContainer.setVisible(false);
+    this.listHelper.setVisible(false);
     this.eraseCursor();
   }
 
